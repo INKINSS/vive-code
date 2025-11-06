10b6f52110d863e0c4c6f9f7a29d052aa4239ffc
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-11-06 14:26:57 -0600

    feat(layout): añade navBar al layout principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './globals.css'
+import NavBar from '@/components/navBar/NavBar'
 
-const rootLayout = ({ children }: { children: React.ReactNode }) => {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html className="bg-customWhite" lang="es">
-      <body>{children}</body>
+      <head>
+        <title>viveCode</title>
+      </head>
+      <body className="px-6 py-4 overflow-hidden">
+        <NavBar />
+        {children}
+      </body>
     </html>
   )
 }
 
-export default rootLayout
+export default RootLayout
