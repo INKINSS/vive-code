15950eb71eee7dfd0e4b908ef421a0b95cc3ed3d
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-11-06 15:27:08 -0600

    feat(components): añade boton de acción para redirigir a nueva pagina

---
+++ src/components/common/actionButtons/ActionButton.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const ActionButton = ({ text, href }: { text: string; href: string }) => {
+  return (
+    <Link
+      href={href}
+      className="bg-customWhite text-customMainBlack text-sm border border-customBlack text-customBlack px-4 py-3 hover:bg-customMainBlack hover:text-customWhite transition-colors"
+    >
+      {text}
+    </Link>
+  );
+};
+
+export default ActionButton;
