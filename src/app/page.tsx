49fdf6541a34243635253eb054cf6d7d2f91a96c
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-11-06 15:30:27 -0600

    feat(pages): añade banner principal de ruta de inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import ActionButton from "@/components/common/actionButtons/ActionButton"
+
 const Home = () => {
-  return <h2>Home</h2>
+  return (
+    <header>
+      <h1 className="mt-16 font-semibold text-xl md:text-4xl">Recorre el mundo del Desarrollo</h1>
+      <p className="w-full md:w-1/2 mt-2 text-customGray">aprende el gran mundo del desarrollo en base al lenguaje Javascript, conocee HTML, CSS, React y mucho más. Este es un espacio que te mostrará el camino de un desarrollador hoy en día, acompañado con documentación, ejercicios y recursos útiles.</p>
+      <div className="mt-6">
+      <ActionButton href="/blocks" text="Explorar Bloques" />
+      </div>
+    </header>
+  )
 }
 
 export default Home
