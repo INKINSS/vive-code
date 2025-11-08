24d635c2ad45d62472f93784e2c463718f7fc8ef
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-11-07 22:24:25 -0600

    refactor(pages): utiliza el componente del banner principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import ActionButton from "@/components/common/actionButtons/ActionButton"
+import MainBanner from "@/components/mainBanner/MainBanner"
 
 const Home = () => {
   return (
-    <header>
-      <h1 className="mt-16 font-semibold text-xl md:text-4xl">Recorre el mundo del Desarrollo</h1>
-      <p className="w-full md:w-1/2 mt-2 text-customGray">aprende el gran mundo del desarrollo en base al lenguaje Javascript, conocee HTML, CSS, React y mucho más. Este es un espacio que te mostrará el camino de un desarrollador hoy en día, acompañado con documentación, ejercicios y recursos útiles.</p>
-      <div className="mt-6">
-      <ActionButton href="/blocks" text="Explorar Bloques" />
-      </div>
-    </header>
+    <MainBanner />
   )
 }
 
