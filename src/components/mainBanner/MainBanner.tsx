3297603c2e569a12637f23d6097447fedb0244c7
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-11-08 21:48:22 -0600

    feat(components): añade seccion informativa al banner principal

--- src/components/mainBanner/MainBanner.tsx
+++ src/components/mainBanner/MainBanner.tsx
@@ -1,20 +1,22 @@
-import ActionButton from "../common/actionButtons/ActionButton";
+import PrimaryButton from "../common/buttons/PrimaryButton";
+import SecundaryButton from "../common/buttons/SecundaryButton";
 
 const MainBanner = () => {
   return (
     <header className="flex items-center pb-10 mt-6 border-b border-customGray md:border-none">
-      <div className="flex-1">
-        <h1 className="mt-16 font-semibold text-xl md:text-4xl">
-          Recorre el mundo del Desarrollo
+      <div className="flex flex-col items-center text-center w-full px-4 md:px-0">
+        <h1 className="mt-16 font-semibold text-xl md:text-4xl animate-blurUp text-balance lg:w-3/4">
+          Recorre el mundo del Desarrollo todo en un solo lugar
         </h1>
-        <p className="w-full md:w-1/2 mt-2 text-customGray text-sm">
+        <p className="w-full md:w-3/4 lg:w-1/2 mt-2 text-customGray text-sm animate-blurUp delay-100">
           aprende el gran mundo del desarrollo en base al lenguaje Javascript,
           conocee HTML, CSS, React y mucho más. Este es un espacio que te
           mostrará el camino de un desarrollador hoy en día, acompañado con
           documentación, ejercicios y recursos útiles.
         </p>
-        <div className="mt-6">
-          <ActionButton href="/blocks" text="Explorar Bloques" />
+        <div className="flex gap-3 mt-6 animate-blurUp delay-200">
+          <PrimaryButton href="/blocks" text="Explorar Bloques" />
+          <SecundaryButton href="/about" text="Sobre Nosotros" />
         </div>
       </div>
     </header>
