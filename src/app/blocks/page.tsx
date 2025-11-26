e005eedc8d919949de5a94c303cf187a5ea9b29f
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-11-25 19:11:40 -0600

    feat(pages): añade bloque HTML a pagina de bloques

--- src/app/blocks/page.tsx
+++ src/app/blocks/page.tsx
@@ -1,5 +1,20 @@
-const Blocks = () => {
-  return <h2>blocks</h2>
-}
+import CardBlock from "@/components/cardBlock/CardBlock";
 
-export default Blocks
+const Blocks = () => {
+  return (
+    <main className="mt-10">
+      <h1 className="text-center py-10 text-2xl font-bold animate-blurUp">
+        Listado de bloques
+      </h1>
+      <article className="flex container mx-auto flex-col md:flex-row flex-wrap gap-4 animate-blurUp-delay-1 mb-10">
+        <CardBlock
+          title="<Introducción a HTML />"
+          description="Aprende los conceptos básicos de HTML, concepto de etiquetas, estructura de un documento HTML y más."
+          href="/blocks/html"
+        />
+      </article>
+    </main>
+  );
+};
+
+export default Blocks;
